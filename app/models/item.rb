688583f66536aca3593bc240417b4c64eb17c9a6
vincent1931098 <1931098.vincent@uib.edu>
2022-04-23 06:09:00 +0700

    Design for Item#index & _item

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,8 @@ class Item < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :price, numericality: { greater_than: 0.01 }
     validates :description,  length: {maximum: 150}
+
+    def get_categories
+        self.categories.pluck(:name).join(', ')
+    end
 end
