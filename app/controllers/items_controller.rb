b1992cd875b7d3e7535028b796258eb4638d880d
vincent1931098 <1931098.vincent@uib.edu>
2022-04-23 07:12:38 +0700

    Can add Category in Food & fix typo

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -65,6 +65,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :price, :description)
+      params.require(:item).permit(:name, :price, :description, category_ids:[])
     end
 end
